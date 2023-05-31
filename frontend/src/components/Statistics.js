63faa5fa685df58f623ecb597145cceb199cd135
marcusmuntean <xhm3ws@virginia.edu>
2023-05-31 14:33:20 -0400

    song and artist card

--- frontend/src/components/Statistics.js
+++ frontend/src/components/Statistics.js
@@ -1,4 +1,5 @@
 import { Box, Card, CardContent, Typography } from "@mui/material";
+import Divider from "@mui/material/Divider";
 
 export default function Statistics() {
   return (
@@ -22,6 +23,8 @@ export default function Statistics() {
               <Typography sx={{ mb: 1.5 }} color="text.secondary">
                 Last week/month/year
               </Typography>
+              <SongCard />
+              <SongCard />
             </CardContent>
           </Card>
         </Box>
@@ -32,10 +35,46 @@ export default function Statistics() {
               <Typography sx={{ mb: 1.5 }} color="text.secondary">
                 Last week/month/year
               </Typography>
+              <ArtistCard />
+              <ArtistCard />
             </CardContent>
           </Card>
         </Box>
       </div>
     </>
+  );
+}
+
+function SongCard() {
+  return (
+    <Card variant="outlined">
+      <CardContent>
+        <Typography variant="h4">1</Typography>
+        <Box marginLeft={"34%"} marginBottom={"1.2%"}>
+          <Divider width={"45%"} />
+        </Box>
+        <Typography variant="h5">Power Trip</Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          J. Cole
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+function ArtistCard() {
+  return (
+    <Card variant="outlined">
+      <CardContent>
+        <Typography variant="h4">1</Typography>
+        <Box marginLeft={"34%"} marginBottom={"1.2%"}>
+          <Divider width={"45%"} />
+        </Box>
+        <Typography variant="h5">Jermaine Cole</Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          Hrs?
+        </Typography>
+      </CardContent>
+    </Card>
   );
 }
