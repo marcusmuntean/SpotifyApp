b4d3b679ed8264fe144428f134a9c1e71815cd41
Mohammed Alwosaibi <hst6pw@virginia.edu>
2023-06-01 14:56:49 -0400

    Implemented Discovery

--- backend/routes/discovery.js
+++ backend/routes/discovery.js
@@ -6,7 +6,7 @@ const { getDocs, collection } = require("firebase/firestore");
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   allDocData = [];
-  const userDocs = await getDocs(collection(db, "Users"));
+  const userDocs = await getDocs(collection(db, "UsersTesting"));
   userDocs.forEach((userDoc) => {
     allDocData.push(userDoc.data());
   })
--- frontend/src/components/Discovery/Discovery.js
+++ frontend/src/components/Discovery/Discovery.js
@@ -9,16 +9,16 @@ function Discovery() {
   useEffect(() => {
     axios.get("http://localhost:9000/discovery/")
     .then((res) => {
-      console.log(res.data.result)
+      const profiles = document.getElementById("profiles")
+      profiles.innerHTML = ""
       for (let i = 0; i < res.data.result.length; i++) {
         if (res.data.result[i].public) {
-          console.log(i);
           const profile = document.createElement("div");
           profile.className = "profile";
           const nameHeader = document.createElement("h2");
           nameHeader.innerHTML = res.data.result[i].name;
           profile.appendChild(nameHeader)
-          document.getElementById("profiles").appendChild(profile);
+          profiles.appendChild(profile);
         }
       }
     })
