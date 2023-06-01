e8403e594fe23cae3db7216f6244ef675023f3e5
Mohammed Alwosaibi <hst6pw@virginia.edu>
2023-06-01 14:43:25 -0400

    Added discovery.js

---
+++ backend/routes/discovery.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+const db = require("./firebase");
+const { getDocs, collection } = require("firebase/firestore");
+
+/* GET home page. */
+router.get('/', async function(req, res, next) {
+  allDocData = [];
+  const userDocs = await getDocs(collection(db, "Users"));
+  userDocs.forEach((userDoc) => {
+    allDocData.push(userDoc.data());
+  })
+  console.log(allDocData)
+  res.json({result: allDocData});
+});
+
+module.exports = router;
